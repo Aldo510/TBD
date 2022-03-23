46ac7973066ea68553a3f40913779ac9a474ea42
Aldo510 <aldo.619_@ciencias.unam.mx>
2022-03-23 16:37:47 -0600

    add properties model, controller and crud

---
+++ app/controllers/properties_controller.rb
@@ -0,0 +1,57 @@
+class PropertiesController < ApplicationController
+  def index
+    unless user_signed_in?
+      redirect_to root_path
+    end
+  end
+
+  def show
+    @property = Property.find(params[:id])
+  end
+
+  def new
+    @property = Property.new
+  end
+
+  def create
+    @property= current_user.properties.build(property_params)
+    if @property.save
+      flash[:success] = "Se creó la propiedad correctamente"
+      redirect_to properties_index_path
+    else
+      flash[:danger] = "hubó un error al crear la propiedad"
+      redirect_to properties_index_path
+    end
+  end
+
+  def destroy
+    @property = Property.find(params[:id])
+    if @property.destroy
+      flash[:success] = "Se borró la propiedad exitosamente"
+      redirect_to properties_index_path
+    else
+      flash[:danger] = "Hubó un problema al borrar la propiedad"
+      redirect_to properties_index_path
+    end
+
+    
+  end
+
+  def update
+    @property= Property.find(params[:id])
+    if @property.update(property_params)
+      flash[:success] = "Se creó la propiedad correctamente"
+      redirect_to properties_index_path
+    else
+      flash[:danger] = "hubó un error al crear la propiedad"
+      redirect_to properties_index_path
+    end
+  end
+
+  private
+
+  def property_params
+    params.require(:property).permit(:name, :description, :image, :address, :rating)
+  end
+
+end
